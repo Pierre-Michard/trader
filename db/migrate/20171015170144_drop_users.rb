99df07f90324c75bc9f40c1ae7e8eae791162e8f
Pierre Michard <pmich35@gmail.com>
2017-10-15 19:42:49 +0200

    fix drop users table migration

--- db/migrate/20171015170144_drop_users.rb
+++ db/migrate/20171015170144_drop_users.rb
@@ -1,5 +1,11 @@
 class DropUsers < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :users
+  def up
+    if ActiveRecord::Base.connection.table_exists? :users
+      drop_table :users
+    end
+  end
+
+  def down
+
   end
 end
