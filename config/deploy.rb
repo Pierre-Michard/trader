9e1b87a23c3ed5ea5db48892179b99caf03ada89
Pierre Michard <pmich35@gmail.com>
2017-12-16 18:53:56 +0100

    add gdax.yml secret file

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/trader/app"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/secret/kraken.yml", "config/secret/paymium.yml", '.env'
+append :linked_files, "config/secret/kraken.yml", "config/secret/paymium.yml", "config/secret/gdax.yml", '.env'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "node/node_modules"
