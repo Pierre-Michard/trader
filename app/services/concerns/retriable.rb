d2eb61a02ae96e7866e94bde1cd0bea785c640cd
Pierre Michard <pmich35@gmail.com>
2020-12-01 21:47:56 +0100

    fix: Retry placing orders

--- app/services/concerns/retriable.rb
+++ app/services/concerns/retriable.rb
@@ -6,6 +6,7 @@ module Retriable
   rescue => e
     Rails.logger.warn("#{caller[0][/`.*'/][1..-2]}: an #{e.class} exception occurred: #{e.message}")
     if nb_retries > 0
+      sleep(0.2)
       nb_retries = nb_retries - 1
       retry
     else
--- app/services/paymium_service.rb
+++ app/services/paymium_service.rb
@@ -150,7 +150,7 @@ class PaymiumService
   end
 
   def place_limit_order(direction:, btc_amount:, price:)
-    with_retries(nb_retries: 1) do
+    with_retries(nb_retries: 2) do
       res = post('user/orders', {
           type: 'LimitOrder',
           currency: 'EUR',
